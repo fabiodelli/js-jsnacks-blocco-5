eb37fd96c5f8e4b65bde05d4227f82f9a144149e
fabiodelli <ntqdel@hotmail.com>
2023-03-24 12:16:02 +0100

    add array objects

---
+++ snack-1/main.js
@@ -0,0 +1,17 @@
+/* Snack 1:
+Crea un array di 10 oggetti che rappresentano una zucchina, 
+indicando per ognuna varietà, peso e lunghezza.Calcola quanto pesano tutte le zucchine. 
+ */
+
+const zucchine = [
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+  { varieta: 'verde', lunghezza: 1, peso: 1 },
+]
