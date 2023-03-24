2dace291bac4bc930d1a80c30cbb4fb741704f81
fabiodelli <ntqdel@hotmail.com>
2023-03-24 12:26:22 +0100

    modify array add literal

--- snack-1/main.js
+++ snack-1/main.js
@@ -4,16 +4,16 @@ indicando per ognuna varietà, peso e lunghezza.Calcola quanto pesano tutte le z
  */
 
 const zucchine = [
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
-  { varieta: 'verde', lunghezza: 1, peso: 1 },
+    { varietà: 'Zucchino nero di Milano', peso: 0.2, lunghezza: 15 },
+    { varietà: 'Zucchino romanesco', peso: 0.3, lunghezza: 18 },
+    { varietà: 'Zucchino siciliano', peso: 0.25, lunghezza: 14 },
+    { varietà: 'Zucchina trombetta di Albenga', peso: 0.4, lunghezza: 20 },
+    { varietà: 'Zucchina bianca triestina', peso: 0.35, lunghezza: 16 },
+    { varietà: 'Zucchina tonda di Piacenza', peso: 0.15, lunghezza: 10 },
+    { varietà: 'Zucchina lunga fiorentina', peso: 0.3, lunghezza: 20 },
+    { varietà: 'Zucchina gialla', peso: 0.2, lunghezza: 12 },
+    { varietà: 'Zucchina striata di Napoli', peso: 0.25, lunghezza: 14 },
+    { varietà: 'Zucchina patisson', peso: 0.35, lunghezza: 18 }
 ]
 
 let totPeso = 0
@@ -22,4 +22,4 @@ zucchine.forEach(zucchina => {
     totPeso += zucchina.peso   
 });
 
-console.log(totPeso)
+console.log(`${totPeso} kg`)
