71b1ee50523aecbe922950680be682671f721aff
fabiodelli <ntqdel@hotmail.com>
2023-03-24 13:05:39 +0100

    add structure

---
+++ snack-2/main.js
@@ -0,0 +1,12 @@
+/* Scrivi una funzione che accetti tre argomenti:
+un array e due numeri ("a" più piccolo di "b" e "b" grande al massimo quanto il numero di elementi dell'array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra "a" e "b" */
+
+ const numbers = [1,2,3,4,5,6,7,8,9,10]
+ console.log(numbers[3]);
+ const valA = 5 
+ const valB = numbers.length
+
+
+
+
